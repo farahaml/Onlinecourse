4f765f99b987c5df6ca4abdc25137ca0b7cf29b3
farah <farah.381.amalia@gmail.com>
2023-02-23 13:55:42 +0700

    Develop Service-Order-Payment : Migration Table Orders & Payment Logs

---
+++ test-midtrans/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lenovo/test-midtrans',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '038e0c17d1b0babacb27d477a27cb74b6949132c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lenovo/test-midtrans' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '038e0c17d1b0babacb27d477a27cb74b6949132c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'midtrans/midtrans-php' => array(
+            'pretty_version' => '2.5.2',
+            'version' => '2.5.2.0',
+            'reference' => 'a1ad0c824449ca8c68c4cf11b3417ad518311d2b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../midtrans/midtrans-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
