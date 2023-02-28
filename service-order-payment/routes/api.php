966fdc3b9e92c0f8792f350b427de825445a36a9
farah <farah.381.amalia@gmail.com>
2023-02-28 10:06:06 +0700

    Develop Service-Order-Payment : Set Up, Create Migration, Create Models, API Create & Get Orders

--- service-order-payment/routes/api.php
+++ service-order-payment/routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Order's Routes
+Route::post('orders', [App\Http\Controllers\OrderController::class, 'create']);
+Route::get('orders', [App\Http\Controllers\OrderController::class, 'index']);
